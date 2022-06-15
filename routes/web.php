b8fb27543034cab41944f91252532ec579f302ad
shin0823 <shinbay0823@gmail.com>
2022-06-15 12:36:44 +0900

    テキストP45

--- routes/web.php
+++ routes/web.php
@@ -22,25 +22,7 @@ Route::get('/', function () {
 //});
 
 
-Route::get('hello/{msg?}', function($msg='no message.') {
-    $html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee;
-   margin:-40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-   <h1>Hello</h1>
-   <p>{$msg}</p>
-   <p>これは、サンプルで作ったページです。</p>
-</body>
-</html>
-EOF;
+Route::get('hello/{id?}/{pass?}', 'HelloController@index');
 
-   return $html;
-});
+
 
