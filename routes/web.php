abd862bb55c245f3f23c69fe24c537958acbff42
shin0823 <shinbay0823@gmail.com>
2022-06-16 15:00:44 +0900

    テキストP63

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ Route::get('/', function () {
     //return '<html><body><h1>Hello</h1><p>This is sample page.
     //</p></body></html>';
 //});
+Route::get('hello','HelloController@index');
 
-
-
-Route::get('hello', 'JissyuController@index');
+//実習用
+Route::get('jissyu2', 'JissyuController@index');
 
 
