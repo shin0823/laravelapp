ea2032851ab808b62ce9b8e28f5b86cfed6d88e2
shin0823 <shinbay0823@gmail.com>
2022-06-24 14:54:34 +0900

    テキスト134

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,9 @@ Route::get('/', function () {
     //return '<html><body><h1>Hello</h1><p>This is sample page.
     //</p></body></html>';
 //});
-Route::get('hello','HelloController@index');
+use App\Http\Middleware\HelloMiddleware;
+
+Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
 
 //実習用
