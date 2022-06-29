908043ac49143e9720480e229e1d1e20445b5071
shin0823 <shinbay0823@gmail.com>
2022-06-29 11:52:51 +0900

    a

--- resources/views/jissyu3_3/index.blade.php
+++ resources/views/jissyu3_3/index.blade.php
@@ -11,24 +11,28 @@ Laravelについて
      <li>特徴</li>
      <li>バージョン</li>
  </ul>
- @section('subview_title')
- @section('menu_title')
+ @component('components.menu')
+ @slot('menu_title')
  特徴
- @endsection
+ @endslot
  
  @slot('menu_content')
  <p>Laravelは、MVCのWebアプリケーション開発用の無料・オープンソースのPHPで書かれたWebアプリケーションフレームワークである。</p>
  @endslot
- @___(5)___
+ @endcomponent
  @endsection
  
  @section('content')
-    <p>開発元：Taylor Otwell</p>
-    <p>初版：2011年6月</p>
- <p>プログラミング言語：PHP</p>
- <p>対応OS：クロスプラットフォーム</p>
- <p>公式サイト：laravel.com</p>
- @___(6)___('___(7)___', ['subview_title'=>'バージョン',
+    <p>開発元:Taylor Otwell</p>
+    <p>初版:2011年6月</p>
+ <p>プログラミング言語:PHP</p>
+ <p>対応OS:クロスプラットフォーム</p>
+ <p>公式サイト:laravel.com</p>
+ @include('components.subview', ['subview_title'=>'バージョン',
  'subview_content'=>'2020年3月より最新バージョンは7。PHPは7.2.5以上。'])
  @endsection
  
+ @section('footer')
+copyright 2020 東京情報クリエイター工学院専門学校
+@endsection
+
