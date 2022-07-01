f8b91e99f5c772bb0cf39bf1e15ed5524a6c360d
shin0823 <shinbay0823@gmail.com>
2022-07-01 12:02:34 +0900

    テキストP193データーベース設定

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,17 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
 use Validator;
+use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
-  
-   public function index(Request $request)
-   {
-       return view('hello.index', ['msg'=>'フォームを入力下さい。']);
-   }
-
-   public function post(HelloRequest $request)
-   {
-       return view('hello.index', ['msg'=>'正しく入力されました！']);
-   }
-
+    public function index(Request $request)
+    {
+       $items = DB::select('select * from people');
+       return view('hello.index', ['items' => $items]);
+    }
 }
