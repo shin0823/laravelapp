e834df035bb342ae863a4abca6e50abece57f5d2
shin0823 <shinbay0823@gmail.com>
2022-07-06 12:29:42 +0900

    テキストP253

--- app/Person.php
+++ app/Person.php
@@ -3,9 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Builder; 
 class Person extends Model
 {
+
     public function getData()
 {
    return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
@@ -18,5 +19,13 @@ public function scopeAgeGreaterThan($query, $n)
 public function scopeAgeLessThan($query, $n)
 {
    return $query->where('age', '<=', $n);
+}
+protected static function boot()
+{
+   parent::boot();
+
+   static::addGlobalScope('age', function (Builder $builder) {
+       $builder->where('age', '>', 20);
+   });
 }
 }
