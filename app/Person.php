741ae9abba76ceb4073730437d4dc51e9cb5b680
shin0823 <shinbay0823@gmail.com>
2022-07-07 14:39:07 +0900

    テキストP281

--- app/Person.php
+++ app/Person.php
@@ -19,8 +19,8 @@ class Person extends Model
    {
       return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
    }
-   public function board()
-{
-   return $this->hasOne('App\Board');
-}
+   public function boards()
+   {
+      return $this->hasMany('App\Board');
+   }
 }
--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -13,11 +13,17 @@
    @foreach ($items as $item)
        <tr>
            <td>{{$item->getData()}}</td>
-           <td>@if ($item->board != null)
-                   {{$item->board->getData()}}
-               @endif
+           <td>
+           @if ($item->boards != null)
+               <table width="100%">
+               @foreach ($item->boards as $obj)
+                   <tr><td>{{$obj->getData()}}</td></tr>
+               @endforeach
+               </table>
+           @endif
            </td>
        </tr>
    @endforeach
    </table>
 @endsection
+
