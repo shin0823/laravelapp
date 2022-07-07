bebb1d9efe3d3fc2f54f12edf62c67a95db64065
shin0823 <shinbay0823@gmail.com>
2022-07-07 14:18:14 +0900

    テキストP278

--- app/Person.php
+++ app/Person.php
@@ -19,4 +19,8 @@ class Person extends Model
    {
       return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
    }
+   public function board()
+{
+   return $this->hasOne('App\Board');
+}
 }
