47aebe326a291ece82daa38127d8c016e115e7f5
shin0823 <shinbay0823@gmail.com>
2022-07-07 12:02:32 +0900

    テキストP259

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,10 @@ Route::get('person', 'PersonController@index');
 Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
 
+Route::get('person/add', 'PersonController@add');
+Route::post('person/add', 'PersonController@create');
+
+
 //実習用
 Route::get('jissyu2', 'JissyuController@index');
 //jissyu3_1
