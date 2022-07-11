82aacff42b86dc5ba21dbed06ec32f335198071d
shin0823 <shinbay0823@gmail.com>
2022-07-11 11:30:56 +0900

    テキストP303

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,12 @@ Route::get('board', 'BoardController@index');
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
 
+Route::resource('rest', 'RestappController');
+
+Route::get('hello/rest', 'HelloController@rest');
+
+Route::get('hello/session', 'HelloController@ses_get');
+Route::post('hello/session', 'HelloController@ses_put');
 
 //実習用
 Route::get('jissyu2', 'JissyuController@index');
